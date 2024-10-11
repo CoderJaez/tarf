7deba4338d117d10021d044aa23926aed0032cdc
JON REY L. LUMAYAG <jaezer.razor@gmail.com>
2024-10-12 07:19:19 +0800

    Integrate channel push

---
+++ src/libs/pusher_channels/serverChannel.ts
@@ -0,0 +1,11 @@
+import Pusher from "pusher";
+
+
+
+const pusher = new Pusher({
+    appId: '1869134',
+    key: 'd13dce0ca416f8835441',
+    secret: '993213e7ddebc4d79bfc',
+    cluster: 'ap1',
+    useTLS: true
+})
